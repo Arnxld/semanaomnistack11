d04274ef7fe6403daaf4ac93976fe54f6e8789f2
Arnxld <pedro.arnold13@gmail.com>
2020-04-06 14:57:12 -0300

    Funcionalidades adicionais

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors')
+const { errors } = require('celebrate')
 const routes = require('./routes')
 
 
@@ -8,5 +9,6 @@ const app = express(); // criando a aplicação
 app.use(cors())
 app.use(express.json()) // assim a aplicação pode interpretar json, transformando em onj js
 app.use(routes)
+app.use(errors())
 
-app.listen(3333)
+module.exports = app
